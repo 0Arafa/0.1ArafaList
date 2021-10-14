68dda4ccfc2c3cc83900906fd639c24a8feb8b8e
0.1Arafa <86897733+0Arafa@users.noreply.github.com>
2021-10-14 06:56:23 -0400

    Update 0.1ArafaList.py

--- 0.1ArafaList.py
+++ 0.1ArafaList.py
@@ -1,3 +1,4 @@
+#Libraries
 import random
 from colorama import *
 import os
@@ -5,7 +6,9 @@ class bcolors:
 	GREEN = '\033[92m'
 	ENDC = '\033[0m'
 	FAIL = '\033[91m'
+#Clear the terminal
 os.system("clear")
+#Banner and about Author
 print(bcolors.GREEN + '''
 
 ░█████╗░░░░░░███╗░░░█████╗░██████╗░░█████╗░███████╗░█████╗░
@@ -25,6 +28,7 @@ print(Fore.YELLOW + '''
 def content():
 	print(Fore.BLUE + ''' ''')
 	try:
+		#File to save
 		ask_file= input("Enter the name of txt file without deleting the content or create a new one,without .txt: ")
 		file= open(ask_file, "a")
 		yourchoice= input("Enter the begging of Sentence(Symbols,Letters,Numbers...): ")
@@ -33,11 +37,11 @@ def content():
 		end= input("Enter anything in the end if you want(Symbols,Letters,Numbers,...: ")
 		ran= input("Do you want to add numbers or guess on phone number? (Y,N): ")
 		list= ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
-		if ran == "Y" or ran == "y" or ran == "Yes" or ran == "yes" or ran == "YES" or ran == "yEs" or ran == "yeS" or ran == "Yep" or ran == "yep" or ran == "YEP":
+		if ran.lower() == "y" or ran.lower() == "yes" or ran.lower() == "yep":
 			call= str(input("Enter the country calling code without + or with + as you like: "))
 			many= int(input("How many numbers you want to add: "))
 			putting= input("Do you want to add the guessing on phone number or numbers in the begging of Sentence or in the end? (begging,end): ")
-			if putting == "end" or putting == "End" or putting == "END" or putting == "ENd" or putting == "EnD" or putting == "enD" or putting == "eNd":
+			if putting.lower() == "end":
 				for i in range(ran1,ran2):
 					for n in list:
 						guess= random.choices(list,k= many)
@@ -67,4 +71,7 @@ def content():
 	except FileNotFoundError:
 		print(bcolors.FAIL + "Make sure you're writing the right path" + bcolors.ENDC)
 		content()
+	except KeyboardInterrupt:
+		print(bcolors.GREEN + "\nBye!!" + bcolors.ENDC)
 content()
+#I hope to be useful for everyone
